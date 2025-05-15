4f9d4ce950b903988b29f33adcf600b0d966bdd8
woojung8405 <woojung8405@gmail.com>
2025-05-15 11:43:01 +0900

    project init

---
+++ src/main/java/com/openplan/coupon/CouponManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.openplan.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openplan/coupon/CouponManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openplan.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
